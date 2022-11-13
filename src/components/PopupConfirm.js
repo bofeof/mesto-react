d7c5d13406ff6aea2435976ee0d217137d341e9c
Polina Che <perhaybe@gmail.com>
2022-11-13 16:00:18 +0300

    feat: close popups by esc, overlay-click

--- src/components/PopupConfirm.js
+++ src/components/PopupConfirm.js
@@ -1,4 +1,39 @@
+import { useEffect } from 'react';
+
 export default function PopupConfirm({ title, buttonConfirmName, isOpen, onClose, onConfirm, card }) {
+
+/** this activity (useEffect) is experimental.
+ * it allows to close popups by esc-key or just by clicking popup-overlay */
+  useEffect(() => {
+
+  const popUpActive = document.querySelector(".popup_opened");
+
+  if (isOpen && popUpActive){
+    popUpActive.addEventListener("click", handleClickClose);
+    document.addEventListener("keydown", handleEscClose);
+  }
+
+  function handleClickClose(evt) {
+    if (
+      evt.target.classList.contains("popup") ||
+      evt.target.classList.contains("popup__container") ||
+      evt.target.classList.contains("popup__img-container")
+    ) {
+      onClose();
+    }
+  }
+
+  function handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      onClose();
+    }
+  }
+
+  return () => {
+    document.removeEventListener("keydown", handleEscClose);
+  };
+  }, [isOpen]);
+
   function approveRemoving() {
     onConfirm(card);
     onClose();
